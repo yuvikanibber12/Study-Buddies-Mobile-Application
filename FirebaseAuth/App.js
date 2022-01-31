6382eb0e9ca9b374aaccb10a67dfc27fdbb803b4
gitMarv23 <46500443+gitMarv23@users.noreply.github.com>
2022-01-31 15:44:56 -0800

    splash screen done - transition

--- FirebaseAuth/App.js
+++ FirebaseAuth/App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
+import SplashScreen from './screens/SplashScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen options={{headerShown: true}} name="Splash" component={SplashScreen} />
         <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
---
+++ FirebaseAuth/screens/SplashScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SplashScreen = () => {
+    <Image source = {require('../assets/splash.png')}
+        style={{width: 1920, height: 1080}}/>
+    return (
+        null
+       )
+}
+
+export default SplashScreen
